137a551e6493fe1acfc0f91df8820761fca3d170
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-07-23 19:45:35 -0400

    Create state for basic info data and pass to component

--- src/components/CvForm.jsx
+++ src/components/CvForm.jsx
@@ -1,14 +1,16 @@
 import FormButton from "./FormButton";
 import BasicInfo from "./BasicInfo";
+import { useState } from "react";
 
 function CvForm() {
+  const [basicInfo, setBasicInfo] = useState({ firstName: "", lastName: "" });
   return (
     <>
       <form>
         <FormButton text="Create CV"></FormButton>
         <FormButton text="Reset"></FormButton>
         <FormButton text="Show Example"></FormButton>
-        <BasicInfo />
+        <BasicInfo data={basicInfo} setData={setBasicInfo} />
       </form>
     </>
   );
