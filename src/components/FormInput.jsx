6ca08e0c077356fea3e2e21c2e7d4da66268e452
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-07-23 19:37:04 -0400

    Use labelFor to set input id

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -2,7 +2,7 @@ function FormInput({ labelText, labelFor, inputValue, inputType = "text" }) {
   return (
     <>
       <label htmlFor={labelFor}>{labelText}</label>
-      <input value={inputValue} type={inputType} />
+      <input id={labelFor} value={inputValue} type={inputType} />
     </>
   );
 }
