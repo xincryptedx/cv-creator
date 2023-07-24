4685e5abede6bd277ff1d4a1184ddf3c1d456b10
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-07-23 20:30:09 -0400

    Add higher level input onChange handlers

--- src/components/BasicInfo.jsx
+++ src/components/BasicInfo.jsx
@@ -1,17 +1,34 @@
 import FormInput from "./FormInput";
 
 function BasicInfo({ data, setData }) {
-  const { firstName } = data;
+  //Data refs
+  const { firstName, lastName } = data;
+  // Data change handlers
   function setDataProperty(event, property) {
     setData({ ...data, [property]: event.target.value });
   }
+
+  function setFirst(event) {
+    setDataProperty(event, "firstName");
+  }
+
+  function setLast(event) {
+    setDataProperty(event, "lastName");
+  }
+
   return (
     <section>
       <FormInput
         labelFor="firstName"
         labelText="First:"
         inputValue={firstName}
-        setDataProperty={setDataProperty}
+        setDataProperty={setFirst}
+      ></FormInput>
+      <FormInput
+        labelFor="lastName"
+        labelText="Last:"
+        inputValue={lastName}
+        setDataProperty={setLast}
       ></FormInput>
     </section>
   );
