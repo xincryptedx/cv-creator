dd01b023bd79d79c118633301ac121fd11d4b0f3
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-07-28 11:43:57 -0400

    Add form buttons

--- src/components/FormControls.jsx
+++ src/components/FormControls.jsx
@@ -1,9 +1,15 @@
 import FormButton from "./FormButton";
+import styles from "../style/FormControls.module.css";
 
 function FormControls() {
   return (
-    <nav>
-      <FormButton></FormButton>
+    <nav className={styles.formControls}>
+      <FormButton className={styles.createButton} text="Create CV"></FormButton>
+      <FormButton className={styles.resetButton} text="Reset"></FormButton>
+      <FormButton
+        className={styles.exampleButton}
+        text="Show Example"
+      ></FormButton>
     </nav>
   );
 }
