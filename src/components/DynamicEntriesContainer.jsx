159cb458f2a2e7386d9ab64425307d1048a73625
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-07-28 15:00:28 -0400

    Return an entry element for each data entry

--- src/components/DynamicEntriesContainer.jsx
+++ src/components/DynamicEntriesContainer.jsx
@@ -1,8 +1,12 @@
-import React from "react";
 import FormButton from "./FormButton";
 import styles from "../style/DynamicEntriesContainer.module.css";
 
-function DynamicEntriesContainer({ children, entriesData, setEntriesData }) {
+function DynamicEntriesContainer({
+  entriesData,
+  setEntriesData,
+  addDataEntry,
+  EntryElement,
+}) {
   function setDataProperty(event, property, entryUid) {
     setEntriesData((previousEntries) => {
       const updatedEntries = [...previousEntries];
@@ -17,13 +21,14 @@ function DynamicEntriesContainer({ children, entriesData, setEntriesData }) {
 
   return (
     <section className={styles.dynamicEntriesContainer}>
-      {React.Children.map(children, (child) =>
-        React.cloneElement(child, {
-          entriesData: entriesData,
-          setEntriesData: setEntriesData,
-          setDataProperty: setDataProperty,
-        })
-      )}
+      {entriesData.forEach((entry) => {
+        return (
+          <EntryElement
+            setDataProperty={setDataProperty}
+            entryData={entry}
+          ></EntryElement>
+        );
+      })}
       <FormButton text="Add Entry"></FormButton>
     </section>
   );
