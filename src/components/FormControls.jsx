fbd6b0f2e99f694b7be3c86a1403b4339ee4d79e
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-07-29 12:18:40 -0400

    Add method for handling create clicks

--- src/components/FormControls.jsx
+++ src/components/FormControls.jsx
@@ -1,10 +1,20 @@
 import FormButton from "./FormButton";
 import styles from "../style/FormControls.module.css";
 
-function FormControls() {
+function FormControls({ setAppState }) {
+  const createClicked = () => {
+    setAppState((previousState) => {
+      return { ...previousState, showForm: false };
+    });
+  };
+
   return (
     <nav className={styles.formControls}>
-      <FormButton className={styles.createButton} text="Create CV"></FormButton>
+      <FormButton
+        className={styles.createButton}
+        text="Create CV"
+        onClick={createClicked}
+      ></FormButton>
       <FormButton className={styles.resetButton} text="Reset"></FormButton>
       <FormButton
         className={styles.exampleButton}
