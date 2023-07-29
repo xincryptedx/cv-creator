71106934af0ebdc5f7dc3b9c8795e9ed1f8967e1
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-07-29 13:00:28 -0400

    Conditionally render form controls

--- src/components/FormControls.jsx
+++ src/components/FormControls.jsx
@@ -1,27 +1,43 @@
 import FormButton from "./FormButton";
 import styles from "../style/FormControls.module.css";
 
-function FormControls({ setAppState }) {
+function FormControls({ appState, setAppState }) {
   const createClicked = () => {
     setAppState((previousState) => {
       return { ...previousState, showForm: false };
     });
   };
 
-  return (
-    <nav className={styles.formControls}>
-      <FormButton
-        className={styles.createButton}
-        text="Create CV"
-        onClick={createClicked}
-      ></FormButton>
-      <FormButton className={styles.resetButton} text="Reset"></FormButton>
-      <FormButton
-        className={styles.exampleButton}
-        text="Show Example"
-      ></FormButton>
-    </nav>
-  );
+  if (appState.showForm === true)
+    return (
+      <nav className={styles.formControls}>
+        <FormButton
+          className={styles.createButton}
+          text="Create CV"
+          onClick={createClicked}
+        ></FormButton>
+        <FormButton className={styles.resetButton} text="Reset"></FormButton>
+        <FormButton
+          className={styles.exampleButton}
+          text="Show Example"
+        ></FormButton>
+      </nav>
+    );
+  else
+    return (
+      <nav className={styles.formControls}>
+        <FormButton
+          className={styles.createButton}
+          text="Edit CV"
+          onClick={createClicked}
+        ></FormButton>
+        <FormButton className={styles.printButton} text="Print"></FormButton>
+        <FormButton
+          className={styles.exampleButton}
+          text="Show Example"
+        ></FormButton>
+      </nav>
+    );
 }
 
 export default FormControls;
