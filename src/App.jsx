81efeae2490f8631fa31cfdaa576073a80271e1f
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-07-29 11:42:20 -0400

    Move data state to App and pass to CvForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 import CvForm from "./components/CvForm";
+import { useState } from "react";
 
 function App() {
+  const [basicInfo, setBasicInfo] = useState({
+    firstName: "",
+    lastName: "",
+    summary: "",
+    phone: "",
+    email: "",
+    address: "",
+  });
+
+  const [educationEntries, setEducationEntries] = useState([]);
+
   return (
     <>
-      <CvForm />
+      <CvForm
+        basicInfo={basicInfo}
+        setBasicInfo={setBasicInfo}
+        educationEntries={educationEntries}
+        setEducationEntries={setEducationEntries}
+      />
     </>
   );
 }
