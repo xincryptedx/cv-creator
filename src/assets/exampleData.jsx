90905d341f25af4e84a0400d0477574a2fdf0a4f
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-07-31 13:22:27 -0400

    Correct line breaks

--- src/assets/exampleData.jsx
+++ src/assets/exampleData.jsx
@@ -3,9 +3,7 @@ const exampleData = {
   basicInfo: {
     firstName: "John",
     lastName: "Smith",
-    summary: `Experienced technology professional with over 7 years of 
-      experience spanning software engineering, machine learning, and full 
-      stack development. `,
+    summary: `Experienced technology professional with over 7 years of experience spanning software engineering, machine learning, and full stack development. `,
     phone: "555-555-5555",
     email: "example@fakemail.com",
     address: `1234 Streen Lane
@@ -34,10 +32,8 @@ const exampleData = {
       companyName: "BitByte Inc.",
       role: "Full Stack Developer",
       tasks: `Design and build responsive front-end web architecture 
-          \nDevelop clean, maintainable and testable back-end code 
-          for web applications and APIs
-          \nImplement responsive design and ensure cross-browser 
-          compatibility across devices`,
+          \nDevelop clean, maintainable and testable back-end code for web applications and APIs
+          \nImplement responsive design and ensure cross-browser compatibility across devices`,
       dateFrom: "Jan 2024",
       dateTo: "Aug 2026",
     },
@@ -45,10 +41,8 @@ const exampleData = {
       uid: "00000001",
       companyName: "NeuraLink Networks",
       role: "Software Engineer",
-      tasks: `Design, develop, test, and maintain software 
-        applications and systems
-        \nWrite clean, scalable and efficient code using programming 
-        languages like Java, Python, C++
+      tasks: `Design, develop, test, and maintain software applications and systems
+        \nWrite clean, scalable and efficient code using programming languages like Java, Python, C++
         \nBuild reusable and reliable components and libraries for future use`,
       dateFrom: "Sept 2026",
       dateTo: "Jan 2028",
@@ -57,8 +51,7 @@ const exampleData = {
       uid: "00000002",
       companyName: "AIdaptive Solutions",
       role: "Machine Learning Engineer",
-      tasks: `Run machine learning tests and experiments, collect results and 
-        identify optimal models
+      tasks: `Run machine learning tests and experiments, collect results and identify optimal models
         \nClean, preprocess and label datasets to prepare them for training models
         \nMonitor models in production and retrain/optimize them as needed`,
       dateFrom: "Jan 2028",
