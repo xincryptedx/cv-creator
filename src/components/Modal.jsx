b734ebd3f6f1028870fc1c168e5131093d4db54c
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-07-31 13:54:41 -0400

    Rename component for general use

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,9 +1,9 @@
 import { createPortal } from "react-dom";
 
-const ConfirmationModal = ({ children }) => {
+const Modal = ({ children }) => {
   const el = document.getElementById("modal-root");
 
   return createPortal(children, el);
 };
 
-export default ConfirmationModal;
+export default Modal;
