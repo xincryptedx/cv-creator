d59a62afc7154771ee5ac860b1e184b015361656
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-07-31 14:21:00 -0400

    Remove node creation

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createPortal } from "react-dom";
 import App from "./App.jsx";
 import "normalize.css";
 import "./style/baseStyles.css";
-
-createPortal(<div id="portal-root" />, document.body);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
