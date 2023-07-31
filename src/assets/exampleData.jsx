980a696c24dacee55a51d8d877342a5a5925d5a4
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-07-31 13:25:27 -0400

    Fix line spacing

--- src/assets/exampleData.jsx
+++ src/assets/exampleData.jsx
@@ -6,9 +6,7 @@ const exampleData = {
     summary: `Experienced technology professional with over 7 years of experience spanning software engineering, machine learning, and full stack development. `,
     phone: "555-555-5555",
     email: "example@fakemail.com",
-    address: `1234 Streen Lane
-      \nCitytown, ST
-      \n13579`,
+    address: `1234 Streen Lane \nCitytown, ST \n13579`,
   },
   educationEntries: [
     {
@@ -31,9 +29,7 @@ const exampleData = {
       uid: "00000000",
       companyName: "BitByte Inc.",
       role: "Full Stack Developer",
-      tasks: `Design and build responsive front-end web architecture 
-          \nDevelop clean, maintainable and testable back-end code for web applications and APIs
-          \nImplement responsive design and ensure cross-browser compatibility across devices`,
+      tasks: `Design and build responsive front-end web architecture \nDevelop clean, maintainable and testable back-end code for web applications and APIs \nImplement responsive design and ensure cross-browser compatibility across devices`,
       dateFrom: "Jan 2024",
       dateTo: "Aug 2026",
     },
@@ -41,9 +37,7 @@ const exampleData = {
       uid: "00000001",
       companyName: "NeuraLink Networks",
       role: "Software Engineer",
-      tasks: `Design, develop, test, and maintain software applications and systems
-        \nWrite clean, scalable and efficient code using programming languages like Java, Python, C++
-        \nBuild reusable and reliable components and libraries for future use`,
+      tasks: `Design, develop, test, and maintain software applications and systems \nWrite clean, scalable and efficient code using programming languages like Java, Python, C++ \nBuild reusable and reliable components and libraries for future use`,
       dateFrom: "Sept 2026",
       dateTo: "Jan 2028",
     },
@@ -51,9 +45,7 @@ const exampleData = {
       uid: "00000002",
       companyName: "AIdaptive Solutions",
       role: "Machine Learning Engineer",
-      tasks: `Run machine learning tests and experiments, collect results and identify optimal models
-        \nClean, preprocess and label datasets to prepare them for training models
-        \nMonitor models in production and retrain/optimize them as needed`,
+      tasks: `Run machine learning tests and experiments, collect results and identify optimal models \nClean, preprocess and label datasets to prepare them for training models \nMonitor models in production and retrain/optimize them as needed`,
       dateFrom: "Jan 2028",
       dateTo: "Current Date",
     },
