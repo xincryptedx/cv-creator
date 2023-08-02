2e46e7f716a0082d575547fcb66950016d641d79
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-08-02 19:55:08 -0400

    Fix typo

--- src/components/AuthorHeader.jsx
+++ src/components/AuthorHeader.jsx
@@ -17,8 +17,11 @@ function AuthorHeader() {
   return (
     <header className={styles.authorHeader}>
       {" "}
-      <p className={styles.authorP}>{hovering ? "Github" : "xIncryptedx"}</p>
-      <a className={styles.git}>
+      <p className={styles.authorP}>{hovering ? "GitHub" : "xIncryptedx"}</p>
+      <a
+        className={styles.git}
+        href="https://github.com/xincryptedx/cv-creator"
+      >
         <img
           className={styles.gitIcon}
           src={gitIcon}
