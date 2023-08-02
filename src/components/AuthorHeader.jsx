0fdfac212d236884a6f73178dccf08aa8a4704c1
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-08-02 19:24:07 -0400

    Render all elements insted of conditionally

--- src/components/AuthorHeader.jsx
+++ src/components/AuthorHeader.jsx
@@ -1,36 +1,16 @@
 import styles from "../style/AuthorHeader.module.css";
 import gitIcon from "../assets/github-mark.svg";
 import doubleDownArrowIcon from "../assets/double-arrow-down.svg";
-import { useState } from "react";
 
 function AuthorHeader() {
-  const [open, setOpen] = useState(false);
-
-  const handleHover = () => {
-    setOpen(true);
-  };
-
-  const handleHoverEnd = () => {
-    setOpen(false);
-  };
-
   return (
-    <header
-      className={styles.authorHeader}
-      onMouseEnter={handleHover}
-      onMouseLeave={handleHoverEnd}
-    >
-      {open === true ? (
-        <>
-          {" "}
-          <p className={styles.authorP}>Created by: xIncryptedx</p>
-          <a className={styles.git}>
-            <img className={styles.gitIcon} src={gitIcon} alt="🐱" />
-          </a>{" "}
-        </>
-      ) : (
-        <img className={styles.downArrowIcon} src={doubleDownArrowIcon}></img>
-      )}
+    <header className={styles.authorHeader}>
+      {" "}
+      <p className={styles.authorP}>Created by: xIncryptedx</p>
+      <a className={styles.git}>
+        <img className={styles.gitIcon} src={gitIcon} alt="🐱" />
+      </a>{" "}
+      <img className={styles.downArrowIcon} src={doubleDownArrowIcon}></img>
     </header>
   );
 }
