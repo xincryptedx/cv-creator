d032f32a5fef1f8281e2cabc1652ef6f4beee484
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-08-02 19:39:05 -0400

    Use state to change text when icon hovered over

--- src/components/AuthorHeader.jsx
+++ src/components/AuthorHeader.jsx
@@ -1,14 +1,31 @@
 import styles from "../style/AuthorHeader.module.css";
 import gitIcon from "../assets/github-mark.svg";
 import doubleDownArrowIcon from "../assets/double-arrow-down.svg";
+import { useState } from "react";
 
 function AuthorHeader() {
+  const [hovering, setHovering] = useState(false);
+
+  const handleHover = () => {
+    setHovering(true);
+  };
+
+  const handleHoverEnd = () => {
+    setHovering(false);
+  };
+
   return (
     <header className={styles.authorHeader}>
       {" "}
-      <p className={styles.authorP}>Created by: xIncryptedx</p>
+      <p className={styles.authorP}>{hovering ? "Github" : "xIncryptedx"}</p>
       <a className={styles.git}>
-        <img className={styles.gitIcon} src={gitIcon} alt="🐱" />
+        <img
+          className={styles.gitIcon}
+          src={gitIcon}
+          alt="🐱"
+          onMouseEnter={handleHover}
+          onMouseLeave={handleHoverEnd}
+        />
       </a>{" "}
       <img className={styles.downArrowIcon} src={doubleDownArrowIcon}></img>
     </header>
