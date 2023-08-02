557e9d23e901f1c0e302956cb59f788ab2266e96
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-08-02 19:08:36 -0400

    Only render children of header conditionally

--- src/components/AuthorHeader.jsx
+++ src/components/AuthorHeader.jsx
@@ -1,25 +1,26 @@
 import styles from "../style/AuthorHeader.module.css";
 import gitIcon from "../assets/github-mark.svg";
+import doubleDownArrowIcon from "../assets/double-arrow-down.svg";
 import { useState } from "react";
 
 function AuthorHeader() {
   const [open, setOpen] = useState(false);
 
-  if (open === true)
-    return (
-      <header className={styles.authorHeader}>
-        <p className={styles.authorP}>Created by: xIncryptedx</p>
-        <a className={styles.git}>
-          <img className={styles.gitIcon} src={gitIcon} alt="" />
-        </a>
-      </header>
-    );
-  else
-    return (
-      <header className={styles.authorHeader}>
-        <p>down arrow</p>
-      </header>
-    );
+  return (
+    <header className={styles.authorHeader}>
+      {open === true ? (
+        <>
+          {" "}
+          <p className={styles.authorP}>Created by: xIncryptedx</p>
+          <a className={styles.git}>
+            <img className={styles.gitIcon} src={gitIcon} alt="🐱" />
+          </a>{" "}
+        </>
+      ) : (
+        <img className={styles.downArrowIcon} src={doubleDownArrowIcon}></img>
+      )}
+    </header>
+  );
 }
 
 export default AuthorHeader;
