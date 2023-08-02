390afb514ef7121c4760a7e2e33728971fd67b34
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-08-02 18:55:18 -0400

    Render based on state

--- src/components/AuthorHeader.jsx
+++ src/components/AuthorHeader.jsx
@@ -1,15 +1,25 @@
 import styles from "../style/AuthorHeader.module.css";
 import gitIcon from "../assets/github-mark.svg";
+import { useState } from "react";
 
 function AuthorHeader() {
-  return (
-    <header className={styles.authorHeader}>
-      <p className={styles.authorP}>Created by: xIncryptedx</p>
-      <a className={styles.git}>
-        <img className={styles.gitIcon} src={gitIcon} alt="" />
-      </a>
-    </header>
-  );
+  const [open, setOpen] = useState(false);
+
+  if (open === true)
+    return (
+      <header className={styles.authorHeader}>
+        <p className={styles.authorP}>Created by: xIncryptedx</p>
+        <a className={styles.git}>
+          <img className={styles.gitIcon} src={gitIcon} alt="" />
+        </a>
+      </header>
+    );
+  else
+    return (
+      <header className={styles.authorHeader}>
+        <p>down arrow</p>
+      </header>
+    );
 }
 
 export default AuthorHeader;
